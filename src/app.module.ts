3a15ee2e5ce138676b992a97dfa6ce7534daf535
João Paulo Chagas da Cruz <joaopauloch17@gmail.com>
2024-06-09 15:25:14 -0300

    wip: user crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,25 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { join } from 'path';
 import { UserModule } from './user/user.module';
+import pg from 'pg';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'joao',
+      password: '',
+      database: 'delivery',
+      entities: [],
+      synchronize: true,
+      driver: pg,
+    }),
     GraphQLModule.forRoot<MercuriusDriverConfig>({
       driver: MercuriusDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      graphiql: true,
     }),
     UserModule,
   ],
